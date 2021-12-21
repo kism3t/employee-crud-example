66e9cb30075553ae4ed1ee337b32d3cce8e4c11a
Ufuk Altin <altin.ufuk@gmail.com>
2021-12-21 09:43:46 +0000

    Initial Spring Boot Project Set Up

---
+++ src/main/java/com/kism3t/employeecrudexample/EmployeeCrudExampleApplication.java
@@ -0,0 +1,13 @@
+package com.kism3t.employeecrudexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeCrudExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeCrudExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kism3t/employeecrudexample/EmployeeCrudExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kism3t.employeecrudexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeCrudExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
